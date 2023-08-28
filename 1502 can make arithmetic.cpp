22cb68e58e07c5377e19f99e65fda7e3c84d6ea9
Ayush Jain <96340111+Ayush2966@users.noreply.github.com>
2023-08-28 22:06:49 +0530

    Create 1502 can make arithmetic.cpp

---
+++ 1502 can make arithmetic.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        int n=arr.size();
+       if(n==2){
+           return true;
+       }else{
+           sort(arr.begin(), arr.end());
+           int d=arr[1]-arr[0];
+           for(int i=2; i<n;i++){
+               if((arr[i]-arr[i-1])!=d){
+                   return false;
+               }
+           }
+           return true;
+       }
+    }
+};
