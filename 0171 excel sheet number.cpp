84d64417c97289ad0ea4b36c74afd72e579f7ca5
Ayush Jain <96340111+Ayush2966@users.noreply.github.com>
2023-08-28 12:34:49 +0530

    Update 0171 excel sheet number.cpp

--- 0171 excel sheet number.cpp
+++ 0171 excel sheet number.cpp
@@ -15,3 +15,18 @@ public:
        return -1;
     }
 };
+
+
+
+
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int ans=0, n=columnTitle.size(), powerValue=0;
+        for(int i=n-1;i>=0;i--){
+            // ans+=((columnTitle[i]-'A'+1)* pow(26, powerValue++));
+            ans+=((int(columnTitle[i])-64)* pow(26, powerValue++));
+        }
+        return ans;
+    }
+};
