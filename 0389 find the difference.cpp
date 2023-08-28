f0c737ae6b72af7d32f9d508f551abfd5f67c200
Ayush Jain <96340111+Ayush2966@users.noreply.github.com>
2023-08-28 17:20:26 +0530

    Create 0389 find the difference.cpp

---
+++ 0389 find the difference.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+    //     int n = s.length();
+    //     sort(s.begin(), s.end());
+    //     sort(t.begin(), t.end());
+    
+    //     for(int i=0;i<n; i++){
+    //         if(s[i]!=t[i]){
+    //           return t[i];
+    //         }
+    //     }
+    //    return t[n];
+    int n=s.length();
+    for(int i=0; i<n;i++){
+        t[i+1]+=t[i]-s[i];
+        }
+        return t[n];
+    }    
+};
