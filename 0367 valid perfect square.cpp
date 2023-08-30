0813c6a6b5fd46e43e0a510ba70a9b35e24bc731
Ayush Jain <96340111+Ayush2966@users.noreply.github.com>
2023-08-31 00:49:33 +0530

    Create 0367 valid perfect square.cpp

---
+++ 0367 valid perfect square.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+       for(long i=1;; i++){         
+            if( i*i == num){
+                return true;
+            }
+            if(i*i>num){
+                return false;
+            }
+        }
+        
+    }
+};
+
+
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+       for(long i=1;; i++){         
+            if( i*i == num){
+                return true;
+            }
+            if(i*i>num){
+                return false;
+            }
+        }
+        
+    }
+};
