16f98775fd96c33613a69a0d9a1a9c1dff46cda2
Ayush Jain <96340111+Ayush2966@users.noreply.github.com>
2023-08-30 23:27:04 +0530

    Create 0234 palindrome ll.cpp

---
+++ 0234 palindrome ll.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        if(head==NULL && head->next== NULL){
+            return head;
+        }
+        ListNode *r_head=NULL;
+        ListNode *ptr=head;
+        while(ptr!=NULL){
+            ListNode *temp= new ListNode(ptr->val);
+            temp->next=r_head;
+            r_head=temp;
+            ptr=ptr->next;
+        }
+        while(head && r_head){
+            if(head->val != r_head->val){
+                return false;
+            }
+            head = head->next;
+            r_head = r_head->next;
+        }
+        return true;
+   
+        
+    }
+};
